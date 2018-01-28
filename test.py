27e22559951e582f243e52181b4c54a922cf4a6a
fcj <709124637@qq.com>
2018-01-28 17:01:05 +0800

    '1.0.3'

--- test.py
+++ test.py
@@ -1,15 +1,25 @@
+import math
 maxlen=input('请输入要获取第几个的质数（大于2）：')
-n=2
-x=2
+n=3 #除去小于5的质数1-2-3
+x=4
+def isPrime(num):
+    if num%6!=1 and num%6!=5:
+        return False
+    sqprnum=math.sqrt(num)
+    i=5
+    while(i<=sqprnum):
+        if num%i==0:
+            return False
+        elif num%(i+2)==0:
+            return False
+        else:
+            i+=6
+    return True
+#该算法是以5开始的自然数以6的倍数为规律计算
 while(n<int(maxlen)):
-    isnotnum=0
     x+=1
-    print(n)
-    for i in range(2,x):
-        if x%i==0:
-            isnotnum=1
-            break
-    if isnotnum==0:
+    if isPrime(x)==True:
         n+=1
+
 print('第',n,'个质数')
 print('质数是：',x)
